843d3d2810164951a52aa0c7bdb540f00d51612b
tony deride <tony.deride.lp4@gmail.com>
2020-06-11 14:15:17 +0200

    ingredient Repository

--- src/main/java/fr/simplon/orgamenu/repository/IngredientRepository.java
+++ src/main/java/fr/simplon/orgamenu/repository/IngredientRepository.java
@@ -16,11 +16,14 @@ public interface IngredientRepository extends JpaRepository<Ingredient, Integer>
     @Query("select i from Ingredient i")
     List<Ingredient> findAll();
 
-    //a refactoriser pour les boucles for
+//    a refactoriser pour les boucles for
 //    Select ingredient from co.users, co.calendrier, co.programmer, co.contenir, co.ingredient
 //    where co.users.id=co.calendrier.user_id
 //    and co.programmer.id_calendrier=co.calendrier.id_calendrier
 //    and co.contenir.id_recette= co.programmer.id_recette
 //    and co.contenir.id_ingredient=co.ingredient.id_ingredient
 //    and co.users.id=2
+
+//    @Query("select Ingredient from User, CalendrierRecette,  ")
+//    List<Ingredient> find();
 }
